db2ddda15d4c1756afcfdfc8243d19b45873091b
Brandon <brando5393@yahoo.com>
2013-10-17 21:08:14 -0500

    added index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<div class="jumbotron">
+  <div class="container">
+    <h1>Hello, and welcome</h1>
+    <p>This website’s primary goal is to promote and showcase the work of Mr. Brandon T. Williams as well as provide others with the opportunity/ability to contract Brandon to perform various freelance projects.</p>
+    <p><a class="btn btn-primary btn-lg">Enter Site</a></p>
+  </div>
+</div>
