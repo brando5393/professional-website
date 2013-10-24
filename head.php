68bf7acd03279cad304ad0f9bba15882d6bed385
Logan Schmidt <logo3801@gmail.com>
2013-10-23 21:27:53 -0500

    TYPO :)

--- head.php
+++ head.php
@@ -1,5 +1,5 @@
 <head>
 <title>Brandon Williams</title>
 <link href="./assets/css/bootstrap.css" rel="stylesheet">
-<iink href="./assets/css/animate.css" rel="stylesheet">
+<link href="./assets/css/animate.css" rel="stylesheet">
 </head>
