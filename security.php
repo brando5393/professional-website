7e918294ff2a6e988b6e2a296155b7861a2f05a9
Brandon Williams <brando5393@yahoo.com>
2014-01-17 01:51:02 -0600

    added security.php

---
+++ security.php
@@ -0,0 +1,64 @@
+<html>
+<?php
+include './head.php'
+?>
+<body>
+<?php
+include './nav.php';
+include_once("analyticstracking.php");
+?>
+    <div class="container">
+
+      <div class="row row-offcanvas row-offcanvas-right">
+
+        <div class="col-xs-12 col-sm-9">
+          <p class="pull-right visible-xs">
+            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
+          </p>
+          <div class="jumbotron">
+            <h1>Networking</h1>
+            <p>The process or act of linking (machines, esp. computers) to operate interactively.</p>
+          </div>
+          <div class="row">
+            <div class="col-6 col-sm-6 col-lg-4">
+              <h2>Cisco Routers</h2>
+              <p>Use Cisco Routers or equipment? If so you are in luck, thanks to my CCNA classes at Waukesha County Technical Collage I am adept at installing and configuring Cisco routers and switches.</p>
+            </div><!--/span-->
+            <div class="col-6 col-sm-6 col-lg-4">
+              <h2>Servers</h2>
+              <p>Need a server admin? I'm your man, I have vast experience with installing and maintaining services such as PHPmyadmin, ownCloud, Snort, and Splunk.</p>
+            </div><!--/span-->
+            <div class="col-6 col-sm-6 col-lg-4">
+              <h2>Security</h2>
+              <p>Need help securing your computer or server check out the security section of my website to see what solutions I offer.</p>
+              <p><a class="btn btn-default" href="./security.php" role="button">View Security &raquo;</a></p>
+            </div><!--/span-->
+            <div class="col-6 col-sm-6 col-lg-4">
+			<!--
+              <h2>Heading</h2>
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            </div><!--/span-->
+            <div class="col-6 col-sm-6 col-lg-4">
+              <!--
+			  <h2>Heading</h2>
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            </div><!--/span-->
+            <div class="col-6 col-sm-6 col-lg-4">
+              <!--
+			  <h2>Heading</h2>
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            </div><!--/span-->
+          </div><!--/row-->
+        </div><!--/span-->
+        </div><!--/span-->
+      </div><!--/row-->
+
+      <hr>
+<?php
+include './footer.php'
+?>
+</body>
+</html>
