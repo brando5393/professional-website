e6cd09748ffab4d7c1bc447a6964841c997e8bce
Logan Schmidt <logo3801@gmail.com>
2014-01-19 15:20:44 -0600

    Indenting

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
 <html>
-<?php include './head.php'; ?>
-<?php include_once("./analyticstracking.php"); ?>
-<body>
-<div class="jumbotron">
-  <div class="container">
-    <h1 class="animated bounceInLeft">Hello, and welcome</h1>
-    <p>This website&#39;s primary goal is to promote and showcase the work of Mr. Brandon T. Williams as well as provide you with the opportunity/ability to contract Brandon to perform various freelance projects.</p>
-    <p class="animated tada"><a class="btn btn-primary btn-lg" href="./home.php">Enter Site </a></p>
-  <?php
-include './footer.php';
-?>
-  </div>
-</div>
-</body>
+	<?php include './head.php'; ?>
+	<?php include_once("./analyticstracking.php"); ?>
+	<body>
+		<div class="jumbotron">
+			<div class="container">
+				<h1 class="animated bounceInLeft">Hello, and welcome</h1>
+				<p>This website&#39;s primary goal is to promote and showcase the work of Mr. Brandon T. Williams as well as provide you with the 			opportunity/ability to contract Brandon to perform various freelance projects.</p>
+				<p class="animated tada"><a class="btn btn-primary btn-lg" href="./home.php">Enter Site </a></p>
+				<?php include './footer.php'; ?>
+			</div>
+		</div>
+	</body>
 </html>
