0339cfb81dc11013b4ec3734f2243bd55087171b
Brandon Williams <brando5393@yahoo.com>
2014-01-31 16:32:33 -0600

    Started work on webdesign.php

---
+++ webdesign.php
@@ -0,0 +1,53 @@
+<html>
+<?php
+include  './head.php'
+?>
+<body>
+<?php
+include  './nav.php';
+include_once("analyticstracking.php");
+?>
+<div  class="container">
+
+    <div class="row">
+        <div  class="jumbotron">
+            <h1>Web Design</h1>
+             <p>Web design encompasses many different skills and disciplines in the production and maintenance of websites.</p>
+        </div>
+    </div><!--endrow-->
+
+<div class="row">
+   <div class="col-md-4">
+        <h1>Snort</h1>
+        <p>Snort  is  a  <b>free</b>  open  source  network  intrusion  prevention  and  detection  system  (IDS/IPS).  I  would  be  happy  to  install  and  configure  this  software  for  you  to  help  keep  your  systems  safe.</p>
+    </div>
+    <div class="col-md-4">
+        <h1>Splunk</h1>
+        <p>I  use  Splunk  as  a  front  end  for  monitoring  snort  or  Tshark  captures.  I  highly  recommend  this  product  to  anyone  who  is  not  familiar  with  a  command  line  interface  or  wants  to  view  multiple  sets  of  data  in  one  secure  location.</p>
+    </div>
+    <div class="col-md-4">
+        <h1>Wireshark</h1>
+        <p>Wireshark is a network protocal analyzer that will give in detail information about the individual packets going through the wires of your network. I will preform captures for <b>Research purposes only.</p>
+    </div>
+</div><!--endrow-->
+
+<div class="row">
+   <div class="col-md-4">
+        <h1>Tshark</h1>
+        <p>Snort  is  a  <b>free</b>  open  source  network  intrusion  prevention  and  detection  system  (IDS/IPS).  I  would  be  happy  to  install  and  configure  this  software  for  you  to  help  keep  your  systems  safe.</p>
+    </div>
+    <div class="col-md-4">
+        <h1>Nmap</h1>
+        <p>Nmap is a security scanner used to discover and gain information on the hosts and servers on a network. Some of the information that can be gained via the use of an Nmap scan includes, host discovery, Operating System detection and much more.</p>
+    </div>
+    <div class="col-md-4">
+        <h1>Artillery</h1>
+        <p>Artillery is an advanced active response tool. This <b>free</b> and open source tool works as a honeypot, file system monitor, system hardener and provides real-time threat intelligence feeds for your server(s).</p>
+    </div>
+</div><!--endrow-->
+
+</div><!--close container (dont delete)-->
+<hr>
+<?php include  './footer.php' ?>
+</body>
+</html>
