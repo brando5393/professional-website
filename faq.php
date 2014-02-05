77145f9bcf91e7f9f55b204dfadb57b8ea4a9d9b
Brandon Williams <brando5393@yahoo.com>
2014-02-04 22:22:37 -0600

    added faq.php

---
+++ faq.php
@@ -0,0 +1,69 @@
+<html>
+<?php
+include  './head.php'
+?>
+<body>
+<?php
+include  './nav.php';
+include_once("analyticstracking.php");
+?>
+        <div  class="container">
+
+            <div  class="row  row-offcanvas  row-offcanvas-right">
+
+                <div  class="col-xs-12  col-sm-9">
+                    <p  class="pull-right  visible-xs">
+                        <button  type="button"  class="btn  btn-primary  btn-xs"  data-toggle="offcanvas">Toggle  nav</button>
+                    </p>
+                    <div  class="jumbotron">
+                        <h1>Frequently Asked Questions</h1>
+                        <p>Have questions? Well hopfuly I have the answers. Don't see your question here, please feel free to contact me and ask.</p>
+                    </div>
+                    <div  class="row">
+                        <div  class="col-6  col-sm-6  col-lg-4">
+                            <h2>How much do you charge for your services?</h2>
+                            <p>That depends on what the service is and what you need/want to have done. Always feel free to request a free quote.</p>
+                        </div><!--/span-->
+                        <div  class="col-6  col-sm-6  col-lg-4">
+                            <h2>What is the best way to get in touch with you?</h2>
+                            <p>Head over to the "contact me" page and send me an e-mail or give me a call at (262) 422-6720</p>
+                        </div><!--/span-->
+                        <div  class="col-6  col-sm-6  col-lg-4">
+                            <h2>What is the best time to contact you?</h2>
+                            <p>Monday/Wednesday: 8:00 a.m. - 12:00 p.m. or 4:00 p.m. - 9:30 p.m.</p>
+                            <br>
+                            <p>Tuesday/Thursday: 12:00 p.m. - 9:30 p.m.</p>
+                            <br>
+                            <p>Friday: 9:00 a.m. - 9:30 p.m.</p>
+                            <br>
+                            <p>Saturday/Sunday: No calls will be returned on weekends, any calls made on weekends will be returned during the week.</p>
+                        </div><!--/span-->
+                        <div  class="col-6  col-sm-6  col-lg-4">
+			<!--
+                            <h2>Heading</h2>
+                            <p>Donec  id  elit  non  mi  porta  gravida  at  eget  metus.  Fusce  dapibus,  tellus  ac  cursus  commodo,  tortor  mauris  condimentum  nibh,  ut  fermentum  massa  justo  sit  amet  risus.  Etiam  porta  sem  malesuada  magna  mollis  euismod.  Donec  sed  odio  dui.  </p>
+                            <p><a  class="btn  btn-default"  href="#"  role="button">View  details  &raquo;</a></p>
+                        </div><!--/span-->
+                        <div  class="col-6  col-sm-6  col-lg-4">
+                            <!--
+			    <h2>Heading</h2>
+                            <p>Donec  id  elit  non  mi  porta  gravida  at  eget  metus.  Fusce  dapibus,  tellus  ac  cursus  commodo,  tortor  mauris  condimentum  nibh,  ut  fermentum  massa  justo  sit  amet  risus.  Etiam  porta  sem  malesuada  magna  mollis  euismod.  Donec  sed  odio  dui.  </p>
+                            <p><a  class="btn  btn-default"  href="#"  role="button">View  details  &raquo;</a></p>
+                        </div><!--/span-->
+                        <div  class="col-6  col-sm-6  col-lg-4">
+                            <!--
+			    <h2>Heading</h2>
+                            <p>Donec  id  elit  non  mi  porta  gravida  at  eget  metus.  Fusce  dapibus,  tellus  ac  cursus  commodo,  tortor  mauris  condimentum  nibh,  ut  fermentum  massa  justo  sit  amet  risus.  Etiam  porta  sem  malesuada  magna  mollis  euismod.  Donec  sed  odio  dui.  </p>
+                            <p><a  class="btn  btn-default"  href="#"  role="button">View  details  &raquo;</a></p>
+                        </div><!--/span-->
+                    </div><!--/row-->
+                </div><!--/span-->
+                </div><!--/span-->
+            </div><!--/row-->
+
+            <hr>
+<?php
+include  './footer.php'
+?>
+</body>
+</html>
