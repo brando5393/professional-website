30b9d47d4c33b90555aa6f4a684bb665fba5b2a2
Brandon Williams <brando5393@gmail.com>
2019-08-22 23:47:31 -0500

    move a few more files

---
+++ v3.0/scripts/contactsubmit.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Brandon Williams</title>
+     <meta name="viewport"
+      content="width=device-width,
+      initial-scale=1.0">
+      <!--[if le IE 9]>
+      <script src="http://html5shim.google.com/svn/trunk/html5.js">
+      </script>
+      <![endif]-->
+      <link rel="stylesheet" href="./css/main.css">
+  </head>
+  <body>
+    <div id="wrapper">
+      <header>
+        <h1>Brandon Williams</h1>
+        <h4>Freelance Web Designer</h4>
+      </header>
+      <nav>
+        <a href="index.html">Home</a>
+        <a href="faq.html">FAQs</a>
+        <a href="#">Portfolio</a>
+        <a href="contact.html">Contact Me</a>
+      </nav>
+      <main>
+        <h3>Thank You!</h3>
+        <br>
+        <p> Your inquiry has been sent successfully. Please allow up to 7 to 10 business days for a reply, I look forward to talking with you.</p>
+        <br>
+        <h6>You should be redirected to the home page momentarily, if redirection fails, please select “Home” from the main navigation area to avoid form resubmission or other errors.</h6>
+      </main>
+      <footer>
+        <sub>Brandon Williams</br>Freelance Web Designer</br>E-mail: <a href="mailto:example@example.com?subject=Website Inquiry">example@example.com</a></sub>
+        </br>
+        <div id="copyright">
+          <small><i>Copyright &copy; 2018 Brandon Williams</i></small>
+        </div>
+      </footer>
+    </div>
+  </body>
+<html>
