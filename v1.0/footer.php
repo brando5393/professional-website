dcff4fe8407843cccc735e704080393e75d69e58
Brandon Williams <brando5393@gmail.com>
2020-02-08 14:56:55 -0600

    stopped tracking old site version folders

--- v1.0/footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-Copyright  <?php  echo  date("Y");  ?>  &copy;  Brandon  Williams
-<p  class="pull-right"><a  href="#">Back  to  top</a></p>
-</footer>
--- v1.0/head.php
+++
@@ -1,5 +0,0 @@
-<head>
-	<title>Brandon  Williams</title>
-	<link  href="./assets/css/bootstrap.css"  rel="stylesheet">
-	<link  href="./assets/css/animate.css"  rel="stylesheet">
-</head>
